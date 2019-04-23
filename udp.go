319e59212ffa57c48467d83fac4fd2f5167865aa
Eric Lindau <eric.lindau1@gmail.com>
2019-04-23 01:07:06 -0400

    Initial - plain packets - forked gopacket

---
+++ udp.go
@@ -0,0 +1,91 @@
+package udpfacade
+
+import (
+	"syscall"
+	"net"
+	"github.com/google/gopacket"
+	//"github.com/google/gopacket/layers"
+	"github.com/eric-lindau/gopacket/layers"
+	"encoding/binary"
+	"unsafe"
+	"runtime"
+)
+
+// TODO Add conn closing
+// TODO Determine if pool of sockets might be effective
+
+// A lot of credit to Graham King for this fantastic article:
+// https://www.darkcoding.net/software/raw-sockets-in-go-link-layer/
+
+// NOTE: Requires sudo for raw socket
+func WriteTo(b []byte, src *net.IPAddr, dst *net.UDPAddr) error {
+	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_RAW)
+	if err != nil {
+		return err
+	}
+
+	if err := syscall.SetsockoptInt(fd, syscall.IPPROTO_IP, syscall.IP_HDRINCL, 1); err != nil {
+		return err
+	}
+
+	to := &syscall.SockaddrInet4{
+		Addr: [4]byte{dst.IP[0], dst.IP[1], dst.IP[2], dst.IP[3]},
+		Port: 0,
+	}
+
+	p := gopacket.NewSerializeBuffer()
+	if err := craftPacket(b, &p, src.IP, dst.IP); err != nil {
+		return err
+	}
+
+	if err := syscall.Sendto(fd, p.Bytes(), 0, to); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// TODO UDP & insert bytes
+func craftPacket(b []byte, p *gopacket.SerializeBuffer, src net.IP, dst net.IP) error {
+	opts := gopacket.SerializeOptions{
+		FixLengths: true,
+	}
+
+	ipv4 := layers.IPv4{
+		Version:    4,
+		IHL:        5,
+		SrcIP:      src,
+		DstIP:      dst,
+		TTL:        64,
+		Protocol:   layers.IPProtocolUDP,
+		FragOffset: 16,
+		Flags:      layers.IPv4DontFragment,
+	}
+	if err := gopacket.SerializeLayers(*p, opts, &ipv4); err != nil {
+		panic(err)
+	}
+
+	switch runtime.GOOS { // NOTE: freebsd < version 11 not supported
+	case "darwin", "dragonfly", "openbsd":
+		ipv4.RawSocketByteOrder(*p, nativeEndian)
+	}
+
+	return nil
+}
+
+// https://github.com/tensorflow/tensorflow/blob/master/tensorflow/go/tensor.go#L488-L505
+var nativeEndian binary.ByteOrder
+
+func init() {
+	buf := [2]byte{}
+	*(*uint16)(unsafe.Pointer(&buf[0])) = uint16(0xABCD)
+
+	switch buf {
+	case [2]byte{0xCD, 0xAB}:
+		nativeEndian = binary.LittleEndian
+	case [2]byte{0xAB, 0xCD}:
+		nativeEndian = binary.BigEndian
+	default:
+		panic("Could not determine native endianness.")
+	}
+}
---
+++ udp_test.go
@@ -0,0 +1,27 @@
+package udpfacade
+
+import (
+	"testing"
+	"net"
+)
+
+const (
+	dstPort = 1025
+)
+
+// NOTE: Requires sudo
+func TestWriteTo(t *testing.T) {
+	spoof := &net.IPAddr{
+		IP: net.IPv4(1, 2, 3, 4),
+	}
+	self := &net.IPAddr{
+		IP: net.IPv4(127, 0, 0, 1),
+	}
+
+	err := WriteTo([]byte("a"), spoof, &net.UDPAddr{
+		IP: self.IP, Port: dstPort, Zone: "",
+	})
+	if err != nil {
+		t.Error(err)
+	}
+}
