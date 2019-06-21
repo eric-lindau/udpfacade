af36c12b420e5c57b6a921575d8bc97d2cc85ed8
Eric Lindau <eric.lindau1@gmail.com>
2019-06-21 00:25:24 -0400

    Hotfix: Windows mock implement net.Conn

--- udp_windows.go
+++ udp_windows.go
@@ -18,3 +18,39 @@ type UDPConn struct {
 func DialUDPFrom(src *net.UDPAddr, dst *net.UDPAddr) (*UDPConn, error) {
 	return nil, errors.New("transparent udp connection not implemented for windows")
 }
+
+func (c *UDPConn) Read(b []byte) (int, error) {
+	return -1, errors.New("cannot read from transparent udp connection")
+}
+
+func (c *UDPConn) Write(b []byte) (int, error) {
+	return -1, nil
+}
+
+func (c *UDPConn) Close() error {
+    return nil
+}
+
+func (c *UDPConn) LocalAddr() net.Addr {
+    return nil
+}
+
+func (c *UDPConn) RemoteAddr() net.Addr {
+    return nil
+}
+
+func (c *UDPConn) SetDeadline(t time.Time) error {
+    return nil
+}
+
+func (c *UDPConn) SetReadDeadline(t time.Time) error {
+    return nil
+}
+
+func (c *UDPConn) SetWriteDeadline(t time.Time) error {
+    return nil
+}
+
+func craftPacket(b []byte, p *gopacket.SerializeBuffer, src *net.UDPAddr, dst *net.UDPAddr) error {
+	return nil
+}
